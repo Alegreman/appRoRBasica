413397ff2b3f30e4b70966b91ea8073fc2b0b2ce
jorgealegria <jobaalegriaji@ittepic.edu.mx>
2018-10-21 12:20:40 -0600

    agregue bootstrap

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,3 @@
 class Cliente < ApplicationRecord
     has_many :orden
-    validates :rfc, length: { minimum: 13},
-                presence: true,
-              :nombre_cliente, presence: true
 end
