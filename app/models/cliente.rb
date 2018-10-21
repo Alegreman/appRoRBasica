8d17b76c172dad631189098f4b3392c161a8af90
jorgealegria <jobaalegriaji@ittepic.edu.mx>
2018-10-20 22:01:38 -0600

    agregando validaciones

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,6 @@
 class Cliente < ApplicationRecord
     has_many :orden
+    validates :rfc, length: { minimum: 13},
+                presence: true,
+              :nombre_cliente, presence: true
 end
